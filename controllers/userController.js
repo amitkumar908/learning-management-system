24940246449018baf1f84123e7dea395fe30e727
amitkumar908 <amitkumarbhorayat001@gmail.com>
2025-08-03 23:37:28 +0530

    Educator Model added

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,3 +93,91 @@ export const purchaseCourse = async (req,res) => {
 }
 
 //get , update course progress , add rating
+// Update user Course progress
+
+export const updateUserCourseProgress = async(req,res)=>{
+    try {
+        const userId = req.auth.userId
+        const {courseId, lectureId} = req.body
+        const progressData = await CourseProgress.findOne({userId, courseId})
+
+        if(progressData){
+            if(progressData.lectureCompleted.includes(lectureId)){
+                return res.json({success: true, message: "Lecture Already Completed"})
+            }
+            
+            progressData.lectureCompleted.push(lectureId)
+            progressData.completed = true
+            await progressData.save()
+        }
+        else{
+            await CourseProgress.create({
+                userId,
+                courseId,
+                lectureCompleted: [lectureId]
+
+            })
+        }
+        res.json({success:true, message: 'Progress Updated'})
+    } catch (error) {
+        res.json({success: false, message:error.message})
+    }
+}
+
+// get user course progress
+
+export const getUserCourseProgress = async(req,res)=>{
+    try {
+        const userId = req.auth.userId
+        const {courseId} = req.body
+        const progressData = await CourseProgress.findOne({userId, courseId})
+        res.json({success: true, progressData})
+    } catch (error) {
+        res.json({success: false, message:error.message})
+    }
+}
+
+
+// Add user ratings to course
+
+export const addUserRating = async (req,res)=>{
+    try {
+        const userId = req.auth.userId
+        const {courseId, rating} = req.body
+        // console.log("UserId", courseId);
+        // console.log("courseId", courseId);
+        // console.log("rating", rating);
+        
+
+        if(!courseId || !userId || !rating || rating < 1 || rating > 5)
+        {
+            res.json({success: false, message:"Invalid details"})
+        }
+
+        const course = await Course.findById(courseId)
+        if(!course){
+            return res.json({success: false, message:"Course Not found!"})
+        }
+
+        const user = await User.findById(userId)
+
+        if(!user || !user.enrolledCourses.includes(courseId)){
+            return res.json({success: false, message:"User has not purchased this course."})
+        }
+
+        const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId)
+        if(existingRatingIndex > -1){
+            course.courseRatings[existingRatingIndex].rating = rating;
+        }
+        else{
+            course.courseRatings.push({userId,rating});
+        }
+
+        // await courseData.save()
+        await course.save()
+        res.json({success: true, message:"Rating Added"})
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "../configs/mongoDb.js";
-import courseRoute from "../routes/courseRoute.js";
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinay from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
-
-dotenv.config({});
-
-// call database connection here
-connectDB();
+// initialize express 
 const app = express();
 
+
+// connect to db
+await connectDB();
+await connectCloudinay();
+
+
+// middleware
+app.use(cors());
+app.use(clerkMiddleware())
+
+
+// Routes
+app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+
+
+
+// port
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors());
- 
-// apis
-app.get('/', (req,res)=>{res.send("Eduflow API is working fine!")})
-app.use("/api/course", courseRoute);
-app.use('/api/user', userRouter);
-
- 
- 
-app.listen(PORT, () => {
-    console.log(`Server listen at port ${PORT}`);
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+    
 })
-
